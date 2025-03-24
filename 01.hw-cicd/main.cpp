35711547796cfae7fe32cc66a4fb8c0a600a3ce5
Andrey Kovalenko <frolik0777@mail.ru>
2025-03-24 22:56:37 +0200

    added build num as PATCH_VERSION

--- 01.hw-cicd/main.cpp
+++ 01.hw-cicd/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include "version.h"
 
-static constexpr int BUILD_NUM = 1; 
 
 int main(int, char **) {
-	std::cout << "build " << BUILD_NUM << std::endl;
+	std::cout << "build " << PROJECT_VERSION_PATCH << std::endl;
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
